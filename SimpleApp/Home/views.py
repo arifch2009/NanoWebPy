d9f00273d78781f4fd91a2366b14aeca8a030c90
Arif <arifch2009@gmail.com>
2017-04-14 00:16:48 +0200

    new pages added

--- SimpleApp/Home/views.py
+++ SimpleApp/Home/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+#Add another view for new page
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+

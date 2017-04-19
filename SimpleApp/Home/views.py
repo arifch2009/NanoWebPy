2e1d03e21e97bb5b8838605810bffcb2825c718b
Arif <arifch2009@gmail.com>
2017-04-20 00:13:52 +0200

    changes

--- SimpleApp/Home/views.py
+++ SimpleApp/Home/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from django.http import HttpResponse
- 
+
+
+import docker 
 
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-	text = """<h1>welcome to my app !</h1>"""
+	client = docker.from_env()
+
+	text = ""
+	for i in client.containers.list():
+		print i
+		
+	text = text + """<h1>welcome to my app !</h1>"""
+	#return HttpResponse(text)
 	return HttpResponse(text)
         #return render(request, 'index.html', context=None)  comment: we can response using render also
 

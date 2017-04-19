b33c8031cfed9cbfb8a70889ae06810105aa7e37
Arif <arifch2009@gmail.com>
2017-04-19 23:56:09 +0200

    Views.py is modified

--- SimpleApp/Home/urls.py
+++ SimpleApp/Home/urls.py
@@ -3,7 +3,9 @@
 from django.conf.urls import url
 from Home import views
 
+
+
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),  # This is home page, HomePageView.as_view() is defined inside views.py
+   url(r'^$', views.HomePageView.as_view()),  # This is home page, HomePageView.as_view() is defined inside views.py
     url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route, views.AboutPageView.as_view() is defined inside the views.py
 ]
--- SimpleApp/Home/views.py
+++ SimpleApp/Home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from django.http import HttpResponse
+ 
+
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+	text = """<h1>welcome to my app !</h1>"""
+	return HttpResponse(text)
+        #return render(request, 'index.html', context=None)  comment: we can response using render also
 
 #Add another view for new page
 class AboutPageView(TemplateView):

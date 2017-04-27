62e8a949aaf5976e88f457ccefe3be6c6f1c7a1f
Arif <arifch2009@gmail.com>
2017-04-27 23:59:11 +0200

    now image & container details can be accessed

--- SimpleApp/Home/views.py
+++ SimpleApp/Home/views.py
@@ -12,12 +12,25 @@ class HomePageView(TemplateView):
     def get(self, request, **kwargs):
 	client = docker.from_env()
 
-	text = "This is simple text"
-	 
-	print "Number of the images are : "+ str(len(client.images.list()))
-	print client.images.list()[0]
+	container_text =  "Numner of containers running in this machine : "+ str(len(client.containers.list())) + "<br>Container details : <table border: 1px ><tr><th>Name </th><th>Container ID</th></tr>"
+        for i in client.containers.list():
+		container_text= container_text + "<tr><td>"+ i.name + "</td><td>"+ i.id[0:10] + "</td></tr>"
+
+
+        container_text= container_text +"</table>"
+	
+        
+	image_text =  "Numner of images in this machine : "+ str(len(client.images.list())) + "<br> Detail of the images are : <table border: 1px ><tr><th>Image id</th></tr>"
+        for i in client.images.list():
+		if i.id.startswith('sha256:'):
+			image_text= image_text + "<tr><td>"+ i.id[7:17] + "</td></tr>"
+
+
+        image_text= image_text +"</table>"
+
+	
 	#return HttpResponse(text)
-	return HttpResponse(text)
+	return HttpResponse(container_text+ image_text)
         #return render(request, 'index.html', context=None)  comment: we can response using render also
 
 #Add another view for new page
